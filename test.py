d5d2574274f44a8350a426829fbbae89f59221a3
Mikhail Kalinichenko <m.kalinichenko@expasoft.tech>
2022-02-04 13:40:06 +0700

    add downloading images from yandex

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from YandexImagesParser.ImageParser import YandexImage
+import requests
 
 parser = YandexImage()
 
 print(parser.about, parser.version)
 
-for item in parser.search("Дизайн паттерн"):
+for i, item in enumerate(parser.search("Дизайн паттерн")):
     print(item.title)
     print(item.url)
     print(item.preview.url)
+    img_data = requests.get(item.preview.url).content
+    with open(f'downloads/image_name_{i}.jpg', 'wb') as handler:
+        handler.write(img_data)
     print("(", item.size, ")", sep='')
